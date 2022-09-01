7a14d2efc2243131af8ef4d5f43b67da234e548b
instructor <instructor@learncodinganywhere.com>
2022-09-01 16:21:38 -0500

    added icons, popup modal, lovebutton counter

--- static/javascript.js
+++ static/javascript.js
@@ -1,10 +1,29 @@
-$(function(){
-    $(window).scroll(function() {
-       if($(window).scrollTop() >= 100) {
-         $('.navbar').addClass('scrolled');
-       }
-      else {
-        $('.navbar').removeClass('scrolled');
-      }
-    });
+
+//Navigation bar shrink on scroll
+$(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 100) {
+      $('.navbar').addClass('scrolled');
+    }
+    else {
+      $('.navbar').removeClass('scrolled');
+    }
   });
+});
+
+//Recipe bootstrap modal for featured recipe
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+//Love button counter
+var button = document.getElementById("loveCounter"),
+  count = 0;
+button.onclick = function () {
+  count += 1;
+  button.innerHTML = "<i class='fa-solid fa-heart mx-2'></i> " + count;
+};
+
